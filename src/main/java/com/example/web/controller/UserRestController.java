23948c7dbeafa44493f1dbe4d486339634b15056
ololeg <kalabin-o@mail.ru>
2022-03-10 15:09:38 +0500

    Initial commit

---
+++ src/main/java/com/example/web/controller/UserRestController.java
@@ -0,0 +1,27 @@
+package com.example.web.controller;
+
+import com.example.web.model.User;
+import com.example.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/user")
+public class UserRestController {
+    private final UserService userService;
+
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public User getUserByUsername(Principal principal) {
+        User user = userService.getUserByUsername(principal.getName());
+        return user;
+    }
+}
